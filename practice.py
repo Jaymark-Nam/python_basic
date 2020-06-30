21d0df82fc92d8fbc40269bcd217fdc4633f7554
Jaewoo Nam <hamellnam@gmail.com>
2020-06-30 19:35:04 +0900

    30.06.2020

---
+++ practice.py
@@ -0,0 +1,30 @@
+#num
+print(5)
+print(10*10)
+
+#string
+print('balloon')
+print("balloon")
+print("ha"*4)
+
+#Boolean
+print(5 < 10)
+print(False)
+
+print(not False)
+print(not (5>10))
+
+
+#parameter
+animal = "cat"
+name = "peter"
+age = 4
+hobby = "walking"
+is_ad = age >= 3
+
+print("My "+animal+" is "+name+"")
+print("my "+animal+" "+name+" is "+str(age)+" years old")
+print("my ",animal," ",name," is ",age," years old")
+
+print("she likes "+hobby)
+print("is "+name+" adult? "+str(is_ad))
